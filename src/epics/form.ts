f3e1dd584ace00010698a1dd4a222d66a2a8f484
James Cleveland <james.cleveland@zopa.com>
2019-03-19 14:19:12 +0000

    fix: updating usage of validate

--- src/epics/form.ts
+++ src/epics/form.ts
@@ -1,5 +1,5 @@
 import { ActionsObservable, StateObservable, ofType, combineEpics } from 'redux-observable'
-import * as validate from 'validate.js'
+import { validate } from 'validate.js'
 import { SubmitFormAction, SUBMIT_FORM, postForm, formErrors, POST_FORM } from '../actions'
 import { RootState } from '../interfaces'
 import { map, mapTo } from 'rxjs/operators'
